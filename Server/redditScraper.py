2bcd0994836754ff55f0ed06d1b5e3d844b8d35d
Reshef Elisha <reshef.elisha@gmail.com>
2015-05-17 01:13:37 -0400

    Final commit

--- Server/redditScraper.py
+++ Server/redditScraper.py
@@ -7,6 +7,7 @@ from PIL import Image
 from urllib import urlopen
 import base64, datetime
 import json
+import time, threading
 
 def generateJSON(date, subReddit, title, content, id):
     data = {}
@@ -31,39 +32,40 @@ def cutyCapt(page, Rid):
         return None
     return(htmlstring)
 
-reddit = praw.Reddit('OuternetRedditApp')
-reddit.login('outernet-project','outernet')
-front_page = reddit.get_front_page()
-for submission in front_page:
-    err = 0
-    try:
-        title = str(submission.title)
-        print title
-        subreddit = str(submission.subreddit)
-        date = datetime.datetime.fromtimestamp(int(submission.created_utc)).strftime('%Y-%m-%d %H:%M:%S')
-    except UnicodeEncodeError as e:
-        err = None
-    if err != None:
-        if submission.url.lower().endswith(('.png','.jpg')):
-            image = Image.open(StringIO(urlopen(submission.url).read()))
-            jpeg_image_buffer = StringIO()
-            image.save(jpeg_image_buffer, format="JPEG")
-            img_str = base64.b64encode(jpeg_image_buffer.getvalue())
-            print date
-            jsonObj = generateJSON(date, subreddit, title, img_str, str(submission.id))
-            jsonFile = open('jsonObj/' + submission.id + ".json", 'a')
-            jsonFile.write(str(jsonObj))
-            print 'Printed File'
-        else:
-            img_str = cutyCapt(submission.url, str(submission.id))
-            if img_str != None:
+def App(processedFile):
+    reddit = praw.Reddit('OuternetRedditApp')
+    reddit.login('outernet-project','outernet')
+    front_page = reddit.get_front_page()
+    for submission in front_page:
+        err = 0
+        try:
+            title = str(submission.title)
+            print title
+            subreddit = str(submission.subreddit)
+            date = datetime.datetime.fromtimestamp(int(submission.created_utc)).strftime('%Y-%m-%d %H:%M:%S')
+        except UnicodeEncodeError as e:
+            err = None
+        if err != None:
+            if submission.url.lower().endswith(('.png','.jpg')):
+                image = Image.open(StringIO(urlopen(submission.url).read()))
+                jpeg_image_buffer = StringIO()
+                image.save(jpeg_image_buffer, format="JPEG")
+                img_str = base64.b64encode(jpeg_image_buffer.getvalue())
+                print date
                 jsonObj = generateJSON(date, subreddit, title, img_str, str(submission.id))
-                jsonFile = open('jsonObj/' + str(submission.id) + ".json", 'a')
+                jsonFile = open('jsonObj/' + submission.id + ".json", 'a')
                 jsonFile.write(str(jsonObj))
                 print 'Printed File'
             else:
-                print 'Failed to print File'
-    else:
-        print 'Failed to print File'
-        
-        
+                img_str = cutyCapt(submission.url, str(submission.id))
+                if img_str != None:
+                    jsonObj = generateJSON(date, subreddit, title, img_str, str(submission.id))
+                    jsonFile = open('jsonObj/' + str(submission.id) + ".json", 'a')
+                    jsonFile.write(str(jsonObj))
+                    print 'Printed File'
+                else:
+                    print 'Failed to print File'
+        else:
+            print 'Failed to print File'
+    threading.Timer(6*60*60, App).start()
+App()
