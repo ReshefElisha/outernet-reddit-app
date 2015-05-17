7865214574bf77bacf2158025a4ebe08d8eb362d
MP52 <plunkemd@gmail.com>
2015-05-17 16:21:38 -0400

    Changes to be committed: 	new file:   ../Server/CutyCapt.exe 	new file:   ../Server/cutycapt.py

---
+++ Server/cutycapt.py
@@ -0,0 +1,17 @@
+import subprocess
+import cStringIO
+import PIL.Image
+import os
+
+# assume data contains your decoded image
+
+
+def cutyCapt(page, Rid):
+    website = "--url="+page
+    picture = "--out="+Rid+".png"
+    subprocess.call(["cutycapt.exe", website, picture])
+    with open(Rid+".png", "rb") as f:
+        data = f.read()
+    os.remove(Rid+".png")
+    htmlstring = data.encode("base64").replace('\n','')
+    return('<img src="data:image;base64,'+htmlstring+'">')
